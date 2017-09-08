277ff13579accda948201b01eeed88452b58bfc3
Nicapotato <nick.brooks27@gmail.com>
2017-09-08 04:33:24 -0400

    September 8th

--- NLP/SentDex/twitter.py
+++ NLP/SentDex/twitter.py
@@ -41,4 +41,4 @@ auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["antifa"])
+twitterStream.filter(track=["altright", "Alt-Right"])
--- Neural Nets/Convolutional_neural_network-master/run.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import numpy as np
-import tensorflow as tf
-
-from tensorflow.contrib import learn
-from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
